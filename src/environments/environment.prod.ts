f2c65b3570f729537331085ab3258322f858265f
Carlos <carlosjize@gmail.com>
2022-02-26 16:55:34 -0600

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:'a6c0f15efd8840a48df8eab65c0f6723'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'a6c0f15efd8840a48df8eab65c0f6723'
 };
 
 /*
